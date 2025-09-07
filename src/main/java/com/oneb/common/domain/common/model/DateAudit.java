1aaa1812d0b7e22b452daf3681d509916deca36d
TuyenBui <tuyen.bui@gojitsu.com>
2025-09-07 17:33:15 +0700

    Temporary remove for AI fake created time

--- src/main/java/com/oneb/common/domain/common/model/DateAudit.java
+++ src/main/java/com/oneb/common/domain/common/model/DateAudit.java
@@ -20,7 +20,6 @@ public abstract class DateAudit implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @CreatedDate
     protected Instant createdAt = Instant.now();
 
     @LastModifiedDate
