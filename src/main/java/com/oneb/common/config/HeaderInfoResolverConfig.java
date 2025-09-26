1b41a6896d542ac7758b90c02d87a636e6e64d81
TuyenBui <tuyen.bui@gojitsu.com>
2025-09-26 16:54:14 +0700

    HeaderInfor depend on ExceptionAdvice

--- src/main/java/com/oneb/common/config/HeaderInfoResolverConfig.java
+++ src/main/java/com/oneb/common/config/HeaderInfoResolverConfig.java
@@ -2,18 +2,19 @@ package com.oneb.common.config;
 
 import com.oneb.common.annotation.resolver.HeaderInfoArgumentResolver;
 import com.oneb.common.domain.user.UserInfoService;
+import com.oneb.common.exception.handler.ExceptionAdvice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-@ConditionalOnProperty(name = "app.common.header-info.enabled", havingValue = "true", matchIfMissing = false)
 @Configuration
 @RequiredArgsConstructor
+@ConditionalOnBean(ExceptionAdvice.class)
 public class HeaderInfoResolverConfig implements WebMvcConfigurer {
 
     @Autowired(required = false)
