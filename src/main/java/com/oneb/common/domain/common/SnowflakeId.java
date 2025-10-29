b76b44d29a5edc07fed6485e179c52a0f6610821
TuyenBui <tuyen.bui@gojitsu.com>
2025-10-29 16:35:52 +0700

    Snowflake

---
+++ src/main/java/com/oneb/common/domain/common/SnowflakeId.java
@@ -0,0 +1,13 @@
+package com.oneb.common.domain.common;
+
+import org.hibernate.annotations.IdGeneratorType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@IdGeneratorType(YitterIdGenerator.class)
+public @interface SnowflakeId {}
