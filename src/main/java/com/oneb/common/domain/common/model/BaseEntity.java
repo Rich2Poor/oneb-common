0a0cd87ce4afa2152f23c1f45d4bc1291d04a03e
TuyenBui <tuyen.bui@gojitsu.com>
2026-01-12 10:20:24 +0700

    Base entity

---
+++ src/main/java/com/oneb/common/domain/common/model/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.oneb.common.domain.common.model;
+
+import com.oneb.common.domain.common.SnowflakeId;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @SnowflakeId
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
